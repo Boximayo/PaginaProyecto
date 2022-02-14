c2cc28fcf29966a880a1b2d07453f9fbfe0ebc88
hhectormanuel <hectorcena@live.com.mx>
2022-02-14 10:21:51 -0700

    contacto

--- resources/views/Contactanos/Contactanos.blade.php
+++ resources/views/Contactanos/Contactanos.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
 <h1>Contactanos</h1>
-<div>lado izquierdo</div>
-<p>La miel es una empresa responsable que se interesa en el consumidor, ya que son lo mas importante para nosotros por lo que tenenmos esta seccion de contacto en la cual podras expresar tus dudas o quejas
+<div class="py-8 ...">
+<p class="font-sans">La miel es una empresa responsable que se interesa en el consumidor, ya que son lo mas importante para nosotros por lo que tenenmos esta seccion de contacto en la cual podras expresar tus dudas o quejas
     con nosotros para tener una mejor atencion y ayudarte a resolver todos tus problemas
 </p>
 <p>Email: info@lamiel.com</p>
@@ -12,8 +12,9 @@
 <p>Fax: 311-123-23-23</p>
 <p>Oficinas centro: Av. Allende #23 col centro CP. 63000</p>
 <p>Oficinas COlosio: Av. Luis Donaldo Colosio #55 col el salvador CP. 63025</p>
+</div>
 <div>lado derecho</div>
 <p>tienes una pregunta marcanos</p>
-<img>
+<img src="Imagenes/contacto.png">
 @endsection
 
