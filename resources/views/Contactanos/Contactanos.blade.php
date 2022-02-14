1974f4f31a664a9e2cb1ee14984bbb9900d19cc8
hhectormanuel <hectorcena@live.com.mx>
2022-02-14 10:14:32 -0700

    contactanos

--- resources/views/Contactanos/Contactanos.blade.php
+++ resources/views/Contactanos/Contactanos.blade.php
@@ -2,6 +2,18 @@
 @section('primero', 'Contactanos')
 
 @section('content')
-<h1> ola diablo</h1>
+<h1>Contactanos</h1>
+<div>lado izquierdo</div>
+<p>La miel es una empresa responsable que se interesa en el consumidor, ya que son lo mas importante para nosotros por lo que tenenmos esta seccion de contacto en la cual podras expresar tus dudas o quejas
+    con nosotros para tener una mejor atencion y ayudarte a resolver todos tus problemas
+</p>
+<p>Email: info@lamiel.com</p>
+<p>Telefono: 311-123-23-23</p>
+<p>Fax: 311-123-23-23</p>
+<p>Oficinas centro: Av. Allende #23 col centro CP. 63000</p>
+<p>Oficinas COlosio: Av. Luis Donaldo Colosio #55 col el salvador CP. 63025</p>
+<div>lado derecho</div>
+<p>tienes una pregunta marcanos</p>
+<img>
 @endsection
 
