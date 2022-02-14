75c7697dffc07292ca91fc82a6836137a96b448f
Roberto Flores <roberttozappiain@gmail.com>
2022-02-14 11:11:04 -0700

    Add first draft of some feature

--- resources/views/Contactanos/Contactanos.blade.php
+++ resources/views/Contactanos/Contactanos.blade.php
@@ -2,6 +2,42 @@
 @section('primero', 'Contactanos')
 
 @section('content')
-<h1> ola diablo</h1>
+<div class=" w-full m-5 flex flex-row">
+    <div class="table-header-group ...">
+      <div class="table-row">
+        <div class="table-cell text-left ..."> <h1 class="text-5xl">CONTACTO</h1> <div class="table-cell ..."><p class="font-sans font-bold">La miel es una empresa responsable que se interesa en el consumidor, <br> ya que son lo mas importante para nosotros por lo que tenenmos esta seccion de contacto en la cual podras expresar tus dudas o quejas <br>
+            con nosotros para tener una mejor atencion y ayudarte a resolver todos tus problemas
+        </p></div>        <div class="table-cell ..."><p class="font-sans font-bold m-5"> Email: info@lamiel.com</p>        <div class="table-cell ..."><p class="font-sans font-bold m-5">Telefono: 311-123-23-23</p>        <div class="table-cell ..."><p class="font-sans font-bold m-5">Fax: 311-123-23-23</p>
+
+        </div>
+    </div>
+
+    </div>
+    </div>
+    <div class="table-row-group ">
+      <div class="table-row basis-1/4">
+
+      </div>
+      <div class="table-row basis-1/4">
+
+      </div>
+      <div class="table-row ">
+        </div>
+      </div>
+      <div class="table-row">
+        </div>
+      </div>
+      <div class="table-row">
+        <div class="table-cell ..."><p class="font-sans font-bold m-5">Oficinas centro: Av. Allende #23 col centro CP. 63000</p>
+        </div>
+      </div>
+      <div class="table-row basis-1/2">
+        <div class="table-cell ..."><p class="font-sans font-bold m-5">Oficinas COlosio: Av. Luis Donaldo Colosio #55 col el salvador CP. 63025</p>
+            <div class="table-cell text-left ..."> <img class="object-right" src="/Imagenes/contacto.png" alt=""> </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
